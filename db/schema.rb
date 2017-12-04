eb99e57500e158403f7fa89aaf1f6765288acde2
hortense martin <hortense.mdf@gmail.com>
2017-12-04 18:11:08 +0100

    adding facebook login and forms layout

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171204151349) do
-
+ActiveRecord::Schema.define(version: 20171204162301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +24,6 @@ ActiveRecord::Schema.define(version: 20171204151349) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_registries_on_user_id", using: :btree
     t.index ["wedding_id"], name: "index_registries_on_wedding_id", using: :btree
-
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,6 +39,13 @@ ActiveRecord::Schema.define(version: 20171204151349) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "facebook_picture_url"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
@@ -55,16 +60,12 @@ ActiveRecord::Schema.define(version: 20171204151349) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-
     t.float    "latitude"
     t.float    "longitude"
     t.index ["user_id"], name: "index_weddings_on_user_id", using: :btree
-
   end
 
-
-  add_foreign_key "registries", "user", column: "user_id"
-  add_foreign_key "registries", "wedding", column: "wedding_id"
-  add_foreign_key "weddings", "user", column: "user_id"
-
+  add_foreign_key "registries", "users"
+  add_foreign_key "registries", "weddings"
+  add_foreign_key "weddings", "users"
 end
