16cc7a19e132930f7a8e36057dbe98e67896f6e1
Aurélie Dubuis <aurel.dubs@gmail.com>
2017-12-06 11:47:55 +0100

    Change column in registries

--- db/migrate/20171206101247_change_columns_in_registries.rb
+++
@@ -1,7 +0,0 @@
-class ChangeColumnsInRegistries < ActiveRecord::Migration[5.0]
-  def change
-    add_column :registries, :email, :string
-    add_column :registries, :total, :integer
-    change_column :registries, :guest, :text, array:true, default: []
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206100640) do
+ActiveRecord::Schema.define(version: 20171206101049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,7 +83,6 @@ ActiveRecord::Schema.define(version: 20171206100640) do
     t.datetime "token_expiry"
     t.date     "birthday"
     t.string   "location"
-    t.string   "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
