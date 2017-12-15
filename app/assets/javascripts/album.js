20a37dc3efef639a5a38464b1088bf086094be47
Aurélie Dubuis <aurel.dubs@gmail.com>
2017-12-15 15:01:34 +0100

    Album photo done

--- app/assets/javascripts/album.js
+++ app/assets/javascripts/album.js
@@ -3,25 +3,29 @@ $(document).ready(function() {
     $.attachinary.config.template = '\
       <div>\
           <% for(var i=0; i<files.length; i++){ %>\
-            <img\
-              src="<%= $.cloudinary.url(files[i].public_id, { "version": files[i].version, "format": "jpg", "crop": "fill", "width": 150, "height": 200 }) %>"\
-              alt="" width="150" height="200" />\
+            <div class="flex_block col-xs-12 col-sm-2">\
+              <img\
+                src="<%= $.cloudinary.url(files[i].public_id, { "version": files[i].version, "format": "jpg", "crop": "fill", "width": 150, "height": 200 }) %>"\
+                alt="" width="150" height="200" />\
+              <a href="#" class="remove-photo fa fa-minus-circle" data-remove="<%= files[i].public_id %>"></a>\
+            </div>\
           <% } %>\
         </div>\
     ';
 
-  $(document)
-   .on('drop dragover', function (e) {
-      e.preventDefault();
-    })
-    .on('cocoon:after-insert', function() {
-      $('.attachinary-input').attachinary();
-    })
-    .on('fileuploadstart', function(e) {
-      $(".add_fields").click();
-      $(e.target).parents('.nested-fields').find('.remove_fields').show();
-      $(e.target).parents('.nested-fields').find('.add_plus').hide();
-    });
+  // $(document)
+  //  .on('drop dragover', function (e) {
+  //     e.preventDefault();
+  //   })
+  //   .on('cocoon:after-insert', function() {
+  //     $('.attachinary-input').attachinary();
+  //   })
+  //   .on('fileuploadstart', function(e) {
+  //     $(".add_fields").click();
+  //     $(e.target).parents('.nested-fields').find('.remove_fields').show();
+  //     $(e.target).parents('.nested-fields').find('.add_plus').hide();
+  //   });
 
-  $(".add_fields").click();
+  // $(".add_fields").click();
 } );
+
